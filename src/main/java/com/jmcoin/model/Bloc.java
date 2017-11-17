dae3f2355699fe380c9fc2e11c3404f89d9adeb8
franckfadeur <franck.fadeur@gmail.com>
2017-11-17 11:34:54 +0100

    Block cration
    Creation Bloc + get & set

---
+++ src/main/java/com/jmcoin/model/Bloc.java
@@ -0,0 +1,57 @@
+package com.jmcoin.model;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Class bloc
+ * Represent the bloc containing transactions
+ * @author franckfadeur
+ *
+ */
+public class Bloc {
+	private List<Transaction> transactions;
+	private int difficulty;
+	private long timeCreation;
+	private int size;
+	private String finalHash;
+	private String prevHash;
+	
+	public List<Transaction> getTransactions() {
+		return transactions;
+	}
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
+	}
+	public int getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+	public long getTimeCreation() {
+		return timeCreation;
+	}
+	public void setTimeCreation(long timeCreation) {
+		this.timeCreation = timeCreation;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public String getFinalHash() {
+		return finalHash;
+	}
+	public void setFinalHash(String finalHash) {
+		this.finalHash = finalHash;
+	}
+	public String getPrevHash() {
+		return prevHash;
+	}
+	public void setPrevHash(String prevHash) {
+		this.prevHash = prevHash;
+	}
+}
